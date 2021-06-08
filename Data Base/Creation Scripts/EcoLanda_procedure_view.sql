11347113c885caf95e69b4de61dc39440314d1eb
Joseba <joseba.izaguirre@alumni.mondragon.edu>
2021-06-08 16:41:16 +0200

    Procedure+view

---
+++ Data Base/Creation Scripts/EcoLanda_procedure_view.sql
@@ -0,0 +1,31 @@
+USE EcoLanda;
+
+/*PROCEDURE para calcular la media del precio de venta de un producto*/
+delimiter //
+
+CREATE PROCEDURE mediaProducto (IN producto VARCHAR(50), OUT media FLOAT)
+       BEGIN
+         SELECT AVG(IFNULL(lp.precio_kilo,0)) AS 'Media'
+         FROM lote_producto lp
+            JOIN tipo_producto tp on tp.id = lp.tipo_id
+         WHERE tp.nombre = producto
+;
+       END//
+
+delimiter ;
+
+/*
+CALL mediaProducto('Tomate', @media); -- media de Tomate
+
+
+SELECT @media;/*
++---------+
+| @media |
++---------+
+|  0.333 |
++---------+*/
+
+/* VIEW USUARIOS*/
+CREATE VIEW usuarios.v AS SELECT nombre, apellido FROM usuario;
+/*VIEW HUERTOS*/
+CREATE VIEW huertos.v AS SELECT * FROM huerto;
